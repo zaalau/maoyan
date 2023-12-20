88dc22abe49e5c076120563571f1ef985b02f6f4
zaalau <452607633@qq.com>
2023-12-20 12:20:41 +0800

    视频页更新、礼品定制更新

--- miniprogram/pages/place/index.js
+++ miniprogram/pages/place/index.js
@@ -11,27 +11,46 @@ Page({
       {
         pagename:'kaiyuan',
         textsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/kaiyuantext.webp?sign=019ba2cc6831b7fc7821121b2def2851&t=1701679734',
-        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/kaiyuanpic.webp?sign=7fe0f1ee27c6ddd8c20a5bcd7ff41275&t=1701679720'
+        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/kaiyuanpic.webp?sign=7fe0f1ee27c6ddd8c20a5bcd7ff41275&t=1701679720',
+        bgsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/changdibottombg.webp?sign=deea1b15893d06d843707a581002bbec&t=1701677901'
       },
       {
         pagename:'taixuhu',
         textsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/taixuhutext.webp?sign=7db048957e0ebf63e2c40d7319df4d8d&t=1701680566',
-        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/taixuhupic.webp?sign=572e7050ba659b165e5ab38754692697&t=1701680601'
+        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/taixuhupic.webp?sign=572e7050ba659b165e5ab38754692697&t=1701680601',
+        bgsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/changdibottombg.webp?sign=deea1b15893d06d843707a581002bbec&t=1701677901'
       },
       {
         pagename:'baijinhanjue',
         textsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/baijinhantext.webp?sign=213b8fde666d8c63b0292a189d1da6e3&t=1701680614',
-        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/baijinhanpic.webp?sign=953e6fc68105dd7ac5adf6c20f11ad69&t=1701680648'
+        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/baijinhanpic.webp?sign=953e6fc68105dd7ac5adf6c20f11ad69&t=1701680648',
+        bgsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/changdibottombg.webp?sign=deea1b15893d06d843707a581002bbec&t=1701677901'
       },
       {
         pagename:'aoti',
         textsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotitext.webp?sign=831ba22986805edbee58d22467d28f42&t=1701680659',
-        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotipic.webp?sign=b3988433b07a093ac1d4582bbd6161a6&t=1701680668'
+        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotipic.webp?sign=b3988433b07a093ac1d4582bbd6161a6&t=1701680668',
+        bgsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/changdibottombg.webp?sign=deea1b15893d06d843707a581002bbec&t=1701677901'
       },
       {
         pagename:'lipin',
+        swiperItem:[
+          {
+            src:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinswiper1.webp?sign=215d42ce5a4c3f55f1ec3d770f2341f9&t=1702958784',
+          },
+          {
+            src:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinswiper2.webp?sign=215d42ce5a4c3f55f1ec3d770f2341f9&t=1702958784',
+          },
+          {
+            src:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinswiper3.webp?sign=215d42ce5a4c3f55f1ec3d770f2341f9&t=1702958784',
+          },
+          {
+            src:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinswiper4.webp?sign=215d42ce5a4c3f55f1ec3d770f2341f9&t=1702958784',
+          },
+        ],
         textsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipintext.webp?sign=0796793bbb2308595381e1a3e36a8e77&t=1702531655',
-        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinpic.webp?sign=2bf647658a809a88ba46f5386044067b&t=1702531604'
+        picsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinbottompic.webp?sign=0f5b906cd7c28cf12c14724838a63db0&t=1702955628',
+        bgsrc:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/lipinbg.webp?sign=d8d5cfe33955c02697c2180609b481a1&t=1702958977'
       },
     ]
   },
--- miniprogram/pages/video/index.js
+++ miniprogram/pages/video/index.js
@@ -9,7 +9,7 @@ Page({
     huodongData:{
       bg:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/videoBg.webp?sign=adc9e3d74959d8fd282cb8fae51d3ca9&t=1701760475',
       huodongItem:[
-        {
+        { 
           title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/huodongTitle.webp?sign=93549205b0de7bf8fad8a87a90293364&t=1701760516',
           mp4item:[
             {
@@ -25,15 +25,80 @@ Page({
 
             },
             {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/hdsptlycsm.webp?sign=86e231b4d7e625d37fd35c6e0e080a69&t=1702979249',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/A6uQZf'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/tmxh.webp?sign=46878f1c089887f203a787aa27557869&t=1702979736',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/A6V3d2'
+            },
+            {
               title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/%E8%A7%86%E9%A2%91-05%20%E6%8B%B7%E8%B4%9D.webp?sign=7f6b2b2ded1613161a11a837eaf27112&t=1701784062',
               finderUserName:'sphpnCphXwj3q7h',
               feedId:'token/AijMjH'
 
             },
             
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/ylct.webp?sign=b18c971005bcd070bf5c3fc06480231e&t=1703044306',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/Aa8SGl'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/ykzzldx.webp?sign=a7558374e57301f2c2197b29bbcb44e5&t=1703044463',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/A8iyd2'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/mengxiangtx.webp?sign=a78c3a4968d73ad6aaf4300f3836408f&t=1703044572',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/A9FhzB'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/liehuo.webp?sign=48f8dd8649b622f5f70215d97336251c&t=1702980392',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AeLCHa'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/weilainihao.webp?sign=b76c3c7c915ee213ef66aef8782352e6&t=1702980547',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/Abn21F'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/dream.webp?sign=f1de0e1feedd2bc4137d9322b7e19401&t=1702980669',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AXkjFa'
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/gqcs.webp?sign=0a71b3b9d1d726f29055242bbf555b3d&t=1702980756',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AvDgxw'
+            },
+            
+            
+            
+            
           ]
         },
-        
+        {
+          title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/meitongtitle.webp?sign=81696ccadab83fbc7eca606c492bcb91&t=1702980933',
+          mp4item:[
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/wushi.webp?sign=1f63b674efda1eb80bbe8a2d448df067&t=1702980976',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/A5BrWc'
+
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/qdys.webp?sign=79e363a9ac347ce9fceb2d5fc61e4fc4&t=1702981286',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AAYMjs'
+
+            },
+          ]
+        }
       ]
     },
     mtvData:{
@@ -97,7 +162,70 @@ Page({
             
           ]
         },
-        
+        {
+          title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/hzscssss.webp?sign=31f36087bbc48b6eece3a217810aaa7c&t=1703044752',
+          mp4item:[
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/hdcy.webp?sign=7baff6ad477f1de182d47c6752b36844&t=1703044950',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AelJYs'
+
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/mlxfhxj.webp?sign=467be64228e5fa3dc1de50dabbdb706d&t=1703045056',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AelJYs'
+
+            },
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/mlhdqy.webp?sign=cc9bd1f3e526a2eeed8fb9cdf8cb1f6b&t=1703045248',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/AYG3AV'
+
+            },
+            
+            
+          ]
+        },
+        {
+          title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/xhqzhxzzfj.webp?sign=cb667050d31f26df07f0a6474d91a9c6&t=1703045392',
+          mp4item:[
+            
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/wxpqmlhd.webp?sign=6712d25df5c010832c7fc615178bdf4e&t=1703045433',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/ADSEzV'
+
+            },
+            
+          ]
+        },
+        {
+          title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/mywcy.webp?sign=d240da07fc57ea2de33e28d4313aaef7&t=1703045555',
+          mp4item:[
+            
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/mywcyxbt.webp?sign=dc06e5fe719a53eeed163d7dec2f91ff&t=1703045592',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/ALhKmC'
+
+            },
+            
+          ]
+        },
+        {
+          title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/hzsw.webp?sign=7d226fdc2f115ea2d3af4194c25a001c&t=1703045696',
+          mp4item:[
+            
+            {
+              title:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/ecgs.webp?sign=732c3d1df0d8105aafa8ff6425fb8072&t=1703045709',
+              finderUserName:'sphpnCphXwj3q7h',
+              feedId:'token/A6JYD6'
+
+            },
+            
+          ]
+        },
       ]
     }
   },
