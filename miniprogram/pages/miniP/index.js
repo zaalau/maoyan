1c33c7170d5a0e50321836996802aa796bbcc8e1
zaalau <452607633@qq.com>
2024-04-20 18:25:18 +0800

    只剩替换视频

--- miniprogram/pages/miniP/index.js
+++ miniprogram/pages/miniP/index.js
@@ -9,25 +9,25 @@ Page({
 
       {
         bg: 'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/minipzk.webp?sign=deb5e4074639f7ed1d3495f4c7de6d26&t=1713418387',
-        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotipic.webp?sign=7915e9c68bb026b6d28b06bef8b1b2f0&t=1713419368',
+        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/coverzk.webp?sign=c5abc4da52abe42db5e7713c9329a9a7&t=1713608358',
         finderUserName: 'sphpnCphXwj3q7h',
         feedId: 'token/A69dBAN0f'
       },
       {
         bg: 'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/miniphd.webp?sign=6db12a54c22d45a1e9339978ce69f7ca&t=1713419728',
-        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotipic.webp?sign=7915e9c68bb026b6d28b06bef8b1b2f0&t=1713419368',
+        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/coverhd.webp?sign=0a598f77bab9d3494d9fb87f3c221041&t=1713608374',
         finderUserName: 'sphpnCphXwj3q7h',
-        feedId: 'token/AhnbOJGEZ'
+        feedId: 'token/AAJ8xE9xS'
       },
       {
         bg: 'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/minipnq.webp?sign=ae25851db28c471534cb2959ab4b49b5&t=1713419754',
-        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotipic.webp?sign=7915e9c68bb026b6d28b06bef8b1b2f0&t=1713419368',
+        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/covernq.webp?sign=ee9d96463793f68048f0a97514a07efb&t=1713608392',
         finderUserName: 'sphpnCphXwj3q7h',
         feedId: 'token/AHBJdlVMM'
       },
       {
         bg: 'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/miniphz.webp?sign=95a85f0349406baed5b413e464f7a378&t=1713419740',
-        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/aotipic.webp?sign=7915e9c68bb026b6d28b06bef8b1b2f0&t=1713419368',
+        cover:'https://636c-cloud1-2g8e8bgqa13910e9-1321771972.tcb.qcloud.la/%E5%B7%B2%E5%8E%8B%E7%BC%A9/coverhz.webp?sign=e40ca852abb3c565f1d0cb4e8bbf4d77&t=1713608404',
         finderUserName: 'sphpnCphXwj3q7h',
         feedId: 'token/A2KbB4IpH'
       }
@@ -35,6 +35,52 @@ Page({
   },
   xx(e) {
     console.log(e)
+  },
+
+  setInputValue(e) {
+    const keyname = e.currentTarget.dataset.keyname || e.target.dataset.keyname
+    this.setData({
+      [keyname]: e.detail.value.trim()
+    })
+  },
+  updateInfo() {
+    wx.vibrateShort()
+    wx.showLoading()
+    const { nameT, companyT, numT } = this.data
+    console.log(nameT, companyT, numT)
+    if( nameT!=''&&nameT!=undefined&&companyT!=''&&companyT!=undefined&&numT!=''&&numT!=undefined){
+      wx.cloud.callFunction({
+        name: 'updateInfo',
+        data: {
+          nameT,
+          companyT,
+          numT
+        },
+        success: res => {
+          wx.hideLoading()
+          console.log(res)        
+          wx.showToast({
+            title: '提交成功',
+          })
+          this.setData({
+            nameT:'',
+            companyT:'',
+            numT:'',
+            ifSetInfo: 'true'
+          })
+        },
+        fail: (err) => {
+          wx.hideLoading()
+          console.error(err);
+        }
+      });
+      
+    }else {
+      wx.showToast({
+        title: '请完整填写',
+        icon: 'error'
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
